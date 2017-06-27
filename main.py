b58767c16056cd84fc65ba60a5c5d510badeb593
nalinc <nalin.chhibber@gmail.com>
2017-06-28 02:15:58 +0530

    skill

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import logging
+from random import randint
 from flask import Flask, render_template
-from flask_pymongo import PyMongo
+from pymongo import MongoClient
 from flask_ask import Ask, statement, question, session
 
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = "riddles_db"
-app.config['MONGO3_HOST'] = 'another.host.example.com'
-app.config['MONGO3_PORT'] = 27017
-app.config['MONGO3_DBNAME'] = 'dbname_three'
-mongo = PyMongo(app, config_prefix='MONGO')
+db_client = MongoClient("mongodb://admin:admin@ds137882.mlab.com:37882/riddles")
+db = db_client.riddles
+db_collection = db.riddles
+
+riddles=[]
 
 ask = Ask(app, "/")
 
@@ -16,24 +17,31 @@ logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
 @ask.launch
 def new_game():
+    count = session.attributes['count'] = 0
+    print(riddles[count]["question"])
     welcome_msg = render_template('welcome')
     return question(welcome_msg)
 
 @ask.intent("YesIntent")
 def next_round():
-    # numbers = [randint(0, 9) for _ in range(3)]
-    round_msg = render_template('round', numbers=numbers)
-    session.attributes['numbers'] = numbers[::-1]  # reverse
+    numbers = [randint(0, 9) for _ in range(3)]
+    session.attributes['count'] = 1
+    count = session.attributes['count']
+    print(riddles[count]["question"])
+    round_msg = riddles[count]["question"]
     return question(round_msg)
 
-@ask.intent("AnswerIntent", convert={'first': int, 'second': int, 'third': int})
-def answer(first, second, third):
-    winning_numbers = session.attributes['numbers']
-    if [first, second, third] == winning_numbers:
-        msg = render_template('win')
+@ask.intent("AnswerIntent", convert={'answer': str})
+def answer(answer):
+    count = session.attributes['count']
+    print(riddles[count]["answer"])
+    #winning_numbers = session.attributes['numbers']
+    if riddles[count]["answer"] == answer:
+        msg = render_template('right')
     else:
-        msg = render_template('lose')
+        msg = render_template('wrong')
     return statement(msg)
 
 if __name__ == '__main__':
+    riddles = list(db_collection.find())
     app.run(debug=True)
