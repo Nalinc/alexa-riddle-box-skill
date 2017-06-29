23088b7c2634ea0668813ee6be17e29c2e18c6b3
nalinc <nalin.chhibber@gmail.com>
2017-06-30 02:57:58 +0530

    ditch globals and use session attributes

--- main.py
+++ main.py
@@ -14,15 +14,13 @@ riddles = list(db_collection.find())
 
 ask = Ask(app, "/")
 
-question_index = 0
-incorrect_guesses = 0
-
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
 @ask.launch
 def new_game():
-    global riddles
     welcome_msg = render_template('welcome')
+    session.attributes['question_index'] = 0
+    session.attributes['incorrect_guesses'] = 0
     print('---start---')
     print(riddles)
     print(len(riddles))
@@ -33,55 +31,51 @@ def new_game():
 @ask.intent("RepeatRiddle")
 @ask.intent("YesIntent")
 def next_round():
-    global question_index, riddles
     print('---next question---')
-    print(question_index)
-    if question_index < len(riddles):
-        print(riddles[question_index]["question"])
-        round_msg = riddles[question_index]["question"]
+    print(session.attributes['question_index'])
+    if session.attributes['question_index'] < len(riddles):
+        print(riddles[session.attributes['question_index']]["question"])
+        round_msg = riddles[session.attributes['question_index']]["question"]
         return question(round_msg)
     else:
         round_msg = render_template('gameover')
-        question_index = 0
+        session.attributes['question_index'] = 0
         return statement(round_msg)
 
 
 @ask.intent("AMAZON.StopIntent")
 @ask.intent("NoIntent")
 def end_round():
-    global question_index, riddles
     print('---end round---')
-    print(question_index)
+    print(session.attributes['question_index'])
     round_msg = render_template('gameover')
-    question_index = 0
+    session.attributes['question_index'] = 0
     return statement(round_msg)
 
 
 @ask.intent("AMAZON.HelpIntent")
 def help_requested():
-    global question_index, riddles
     print('---help---')
-    print(question_index)
+    print(session.attributes['question_index'])
     round_msg = render_template('help')
     return question(round_msg)
 
 
 @ask.intent("AnswerIntent", convert={'answer_response': str})
 def answer(answer_response):
-    global question_index, incorrect_guesses, riddles
     print('---answer---')
     print(answer_response)
-    print(question_index)
-    if riddles[question_index]["answer"] == answer_response:
+    print(session.attributes['question_index'])
+    if riddles[session.attributes['question_index']]["answer"] == answer_response:
         msg = render_template('right')
-        question_index = question_index + 1
-        incorrect_guesses = 0
-    elif incorrect_guesses < 2 and riddles[question_index]["hints"][incorrect_guesses]:
-        msg = riddles[question_index]["hints"][incorrect_guesses]
-        incorrect_guesses = incorrect_guesses + 1
+        session.attributes['question_index'] += 1
+        session.attributes['incorrect_guesses'] = 0
+    elif session.attributes['incorrect_guesses'] < 2 and riddles[session.attributes['question_index']]["hints"][session.attributes['incorrect_guesses']]:
+        msg = riddles[session.attributes['question_index']]["hints"][session.attributes['incorrect_guesses']]
+        session.attributes['incorrect_guesses'] += 1
     else:
         msg = render_template('wrong')
-        incorrect_guesses = 0
+        session.attributes['incorrect_guesses'] = 0
     return question(msg)
 
 if __name__ == '__main__':
