a3c022cf0507263ec6a153d445610351ba70664f
nalinc <nalin.chhibber@gmail.com>
2017-06-30 01:31:38 +0530

    add logs

--- main.py
+++ main.py
@@ -22,6 +22,8 @@ logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 @ask.launch
 def new_game():
     welcome_msg = render_template('welcome')
+    print(riddles)
+    print(len(riddles))
     return question(welcome_msg)
 
 @ask.intent("ResumeRiddle")
